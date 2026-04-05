d48bb21bd5463daa77eda57ac0f97ebc4c148eab
ikemi yuki <miilyu237@outlook.jp>
2026-04-05 21:47:59 +0900

    管理者用勤怠一覧画面のスタイル修正

--- src/resources/views/admin/attendances/index.blade.php
+++ src/resources/views/admin/attendances/index.blade.php
@@ -21,7 +21,7 @@
         <div class="table">
             <x-ui.list-table>
                 <x-slot name="header">
-                    <th class="table__header">名前</th>
+                    <th class="table__header--name">名前</th>
                     <th class="table__header">出勤</th>
                     <th class="table__header">退勤</th>
                     <th class="table__header">休憩</th>
@@ -30,7 +30,7 @@
                 </x-slot>
                 @foreach ($rows as $row)
                     <tr class="table__row">
-                        <td class="table__data">{{ $row->name() }}</td>
+                        <td class="table__data--name">{{ $row->name() }}</td>
                         <td class="table__data">{{ $row->clockIn() }}</td>
                         <td class="table__data">{{ $row->clockOut() }}</td>
                         <td class="table__data">{{ $row->breakTime() }}</td>
